928793c37b55cf44b5cc30c201aeeac31d9187c9
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-07-08 17:40:42 +0330

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import warnings
 
 class KDLoss(nn.Module):
     def __init__(self):
@@ -16,6 +15,7 @@ class KDLoss(nn.Module):
         )
         return kd_loss
 
+
 class RCLoss(nn.Module):
     def __init__(self):
         super(RCLoss, self).__init__()
@@ -27,56 +27,14 @@ class RCLoss(nn.Module):
     def forward(self, x, y):
         return (self.rc(x) - self.rc(y)).pow(2).mean()
 
-def compute_active_filters_correlation(filters, m):
-    if torch.isnan(filters).any():
-        print("filters contain NaN")
-        
-    if torch.isinf(filters).any():
-        print("filters contain Inf")
-        
-    if torch.isnan(m).any() :
-        print("Masks contain NaN")
-        
-    if torch.isinf(m).any():
-        print("Masks contain Inf")
-    
-    active_indices = torch.where(m == 1)[0]
-
-    if len(active_indices) < 2:
-        print("Fewer than 2 active filters found")
-        
-    active_filters = filters[active_indices]
-    active_filters_flat = active_filters.view(active_filters.size(0), -1)
-
-    if torch.isnan(active_filters_flat).any() or torch.isinf(active_filters_flat).any():
-        warnings.warn("Active filters contain NaN or Inf values.")
-    
-    mean = torch.mean(active_filters_flat, dim=1, keepdim=True)
-    centered = active_filters_flat - mean
-    cov_matrix = torch.matmul(centered, centered.t()) / (active_filters_flat.size(1) - 1)
-    std = torch.sqrt(torch.var(active_filters_flat, dim=1))
-
-    epsilon = 1e-6  
-    std_outer = std.unsqueeze(1) * std.unsqueeze(0) 
-    correlation_matrix = cov_matrix / (std_outer + epsilon)
-
-    if torch.isnan(correlation_matrix).any():
-        warnings.warn("Correlation matrix contains NaN values.")
-
-    upper_tri = torch.triu(correlation_matrix, diagonal=1)
-    sum_of_squares = torch.sum(torch.pow(upper_tri, 2))
-
-    num_active_filters = len(active_indices)
-    normalized_correlation = sum_of_squares / num_active_filters
-    return normalized_correlation, active_indices
 
 class MaskLoss(nn.Module):
     def __init__(self):
         super(MaskLoss, self).__init__()
 
-    def forward(self, filters, mask):
-        correlation, active_indices = compute_active_filters_correlation(filters, mask)
-        return correlation, active_indices
+    def forward(self, Flops, Flops_baseline, compress_rate):
+        return torch.pow(Flops / Flops_baseline - compress_rate, 2)
+
 
 class CrossEntropyLabelSmooth(nn.Module):
     def __init__(self, num_classes, epsilon):
