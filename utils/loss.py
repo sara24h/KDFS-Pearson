5d56e73d81c3a248aa0203d3d0bea32a9bf3e366
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-07-09 23:12:25 +0330

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
-from .layer import SoftMaskedConv2d
+from model.student.layer import SoftMaskedConv2d
 
 class KDLoss(nn.Module):
     def __init__(self):
