cf4dd7cea6f8f66f2953b9af6d4d0b57f3bfd29b
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-08-06 07:31:35 +0330

    Update test_resnet50_imagenet.sh

--- run/run_resnet50_imagenet/test_resnet50_imagenet.sh
+++ run/run_resnet50_imagenet/test_resnet50_imagenet.sh
@@ -6,6 +6,7 @@ dataset_mode=hardfake
 ckpt_path=/kaggle/input/kdfs-3-khordad-fixed-temp/results/run_resnet50_imagenet_prune1/student_model/finetune_ResNet_50_sparse_best.pt
 device=0
 
+
 CUDA_VISIBLE_DEVICES=$device python main.py \
   --phase test \
   --dataset_dir $dataset_dir \
@@ -15,4 +16,5 @@ CUDA_VISIBLE_DEVICES=$device python main.py \
   --device cuda \
   --arch $arch \
   --test_batch_size 256 \
-  --sparsed_student_ckpt_path $ckpt_path
+  --sparsed_student_ckpt_path $ckpt_path \
+  "$@"
