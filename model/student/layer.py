9fafa9444bc11093b96ec446a09e4100dd045e47
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-08-12 22:49:28 +0330

    Update layer.py

--- model/student/layer.py
+++ model/student/layer.py
@@ -51,7 +51,8 @@ class SoftMaskedConv2d(nn.Module):
 
     def init_mask(self):
         self.mask_weight = nn.Parameter(torch.Tensor(self.out_channels, 2, 1, 1))
-        nn.init.kaiming_normal_(self.mask_weight)
+        nn.init.constant_(self.mask_weight[:, 1, :, :], 5.0)  # لاجیت قوی برای نگه داشتن
+        nn.init.constant_(self.mask_weight[:, 0, :, :], 0.0)  # لاجیت خنثی برای هرس کردن
 
     def compute_mask(self, ticket):
         if ticket:
