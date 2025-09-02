9672ee55c91ac26254220cb51c470e210eafb761
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-09-02 17:31:48 +0330

    Update GoogleNet_sparse.py

--- model/student/GoogleNet_sparse.py
+++ model/student/GoogleNet_sparse.py
@@ -212,114 +212,33 @@ class GoogLeNet_sparse(MaskedNet):
                 [256, 320, 128, 128],
                 [384, 384, 128, 128],
             ]
+        
+        # RENAMED layers to match teacher model (e.g., inception3a)
+        self.inception3a = block(64, filters[0][0], 96, filters[0][1], 16, filters[0][2], filters[0][3])
+        self.inception3b = block(sum(filters[0]), filters[1][0], 128, filters[1][1], 32, filters[1][2], filters[1][3])
 
-        self.inception_a3 = block(
-            64,
-            filters[0][0],
-            96,
-            filters[0][1],
-            16,
-            filters[0][2],
-            filters[0][3],
-        )
-        self.inception_b3 = block(
-            sum(filters[0]),
-            filters[1][0],
-            128,
-            filters[1][1],
-            32,
-            filters[1][2],
-            filters[1][3],
-        )
-
-        self.covert1 = nn.Conv2d(
-            filters[1][0] + filters[1][1] + filters[1][2] + filters[1][3],
-            filters[1][0] + filters[1][1] + filters[1][2] + filters[1][3],
-            kernel_size=1,
-        )
+        self.covert1 = nn.Conv2d(sum(filters[1]), sum(filters[1]), kernel_size=1)
 
         self.maxpool1 = nn.MaxPool2d(3, stride=2, padding=1)
+
+        self.inception4a = block(sum(filters[1]), filters[2][0], 96, filters[2][1], 16, filters[2][2], filters[2][3])
+        self.inception4b = block(sum(filters[2]), filters[3][0], 112, filters[3][1], 24, filters[3][2], filters[3][3])
+        self.inception4c = block(sum(filters[3]), filters[4][0], 128, filters[4][1], 24, filters[4][2], filters[4][3])
+        self.inception4d = block(sum(filters[4]), filters[5][0], 144, filters[5][1], 32, filters[5][2], filters[5][3])
+        self.inception4e = block(sum(filters[5]), filters[6][0], 160, filters[6][1], 32, filters[6][2], filters[6][3])
+
+        self.covert2 = nn.Conv2d(sum(filters[6]), sum(filters[6]), kernel_size=1)
+
         self.maxpool2 = nn.MaxPool2d(3, stride=2, padding=1)
 
-        self.inception_a4 = block(
-            sum(filters[1]),
-            filters[2][0],
-            96,
-            filters[2][1],
-            16,
-            filters[2][2],
-            filters[2][3],
-        )
-        self.inception_b4 = block(
-            sum(filters[2]),
-            filters[3][0],
-            112,
-            filters[3][1],
-            24,
-            filters[3][2],
-            filters[3][3],
-        )
-        self.inception_c4 = block(
-            sum(filters[3]),
-            filters[4][0],
-            128,
-            filters[4][1],
-            24,
-            filters[4][2],
-            filters[4][3],
-        )
-        self.inception_d4 = block(
-            sum(filters[4]),
-            filters[5][0],
-            144,
-            filters[5][1],
-            32,
-            filters[5][2],
-            filters[5][3],
-        )
-        self.inception_e4 = block(
-            sum(filters[5]),
-            filters[6][0],
-            160,
-            filters[6][1],
-            32,
-            filters[6][2],
-            filters[6][3],
-        )
+        self.inception5a = block(sum(filters[6]), filters[7][0], 160, filters[7][1], 32, filters[7][2], filters[7][3])
+        self.inception5b = block(sum(filters[7]), filters[8][0], 192, filters[8][1], 48, filters[8][2], filters[8][3])
 
-        self.covert2 = nn.Conv2d(
-            filters[6][0] + filters[6][1] + filters[6][2] + filters[6][3],
-            filters[6][0] + filters[6][1] + filters[6][2] + filters[6][3],
-            kernel_size=1,
-        )
-
-        self.inception_a5 = block(
-            sum(filters[6]),
-            filters[7][0],
-            160,
-            filters[7][1],
-            32,
-            filters[7][2],
-            filters[7][3],
-        )
-        self.inception_b5 = block(
-            sum(filters[7]),
-            filters[8][0],
-            192,
-            filters[8][1],
-            48,
-            filters[8][2],
-            filters[8][3],
-        )
-
-        self.covert3 = nn.Conv2d(
-            filters[8][0] + filters[8][1] + filters[8][2] + filters[8][3],
-            filters[8][0] + filters[8][1] + filters[8][2] + filters[8][3],
-            kernel_size=1,
-        )
+        self.covert3 = nn.Conv2d(sum(filters[8]), sum(filters[8]), kernel_size=1)
 
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
-        self.linear = nn.Linear(sum(filters[-1]), num_classes)
+        # RENAMED: from linear to fc
+        self.fc = nn.Linear(sum(filters[-1]), num_classes)
 
         self.mask_modules = [m for m in self.modules() if type(m) == SoftMaskedConv2d]
 
@@ -327,41 +246,34 @@ class GoogLeNet_sparse(MaskedNet):
         feature_list = []
 
         out = self.pre_layers(x)
-        # 192 x 32 x 32
-        out = self.inception_a3(out, self.ticket)
-        # 256 x 32 x 32
-        out = self.inception_b3(out, self.ticket)
+        
+        # RENAMED layers in forward pass
+        out = self.inception3a(out, self.ticket)
+        out = self.inception3b(out, self.ticket)
 
         feature_list.append(self.covert1(out))
 
-        # 480 x 32 x 32
         out = self.maxpool1(out)
-        # 480 x 16 x 16
-        out = self.inception_a4(out, self.ticket)
-        # 512 x 16 x 16
-        out = self.inception_b4(out, self.ticket)
-        # 512 x 16 x 16
-        out = self.inception_c4(out, self.ticket)
-        # 512 x 16 x 16
-        out = self.inception_d4(out, self.ticket)
-        # 528 x 16 x 16
-        out = self.inception_e4(out, self.ticket)
+
+        out = self.inception4a(out, self.ticket)
+        out = self.inception4b(out, self.ticket)
+        out = self.inception4c(out, self.ticket)
+        out = self.inception4d(out, self.ticket)
+        out = self.inception4e(out, self.ticket)
 
         feature_list.append(self.covert2(out))
 
-        # 823 x 16 x 16
         out = self.maxpool2(out)
-        # 823 x 8 x 8
-        out = self.inception_a5(out, self.ticket)
-        # 823 x 8 x 8
-        out = self.inception_b5(out, self.ticket)
+
+        out = self.inception5a(out, self.ticket)
+        out = self.inception5b(out, self.ticket)
 
         feature_list.append(self.covert3(out))
 
-        # 1024 x 8 x 8
         out = self.avgpool(out)
         out = out.view(out.size(0), -1)
-        out = self.linear(out)
+        # RENAMED: from linear to fc
+        out = self.fc(out)
         return out, feature_list
 
 
